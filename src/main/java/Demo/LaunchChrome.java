67b0a42356af1eea5452d84486844fc8a141b62a
Komal <garadanil57@gmail.com>
2024-05-05 18:41:55 +0530

    added WebDriver Manager

--- src/main/java/Demo/LaunchChrome.java
+++ src/main/java/Demo/LaunchChrome.java
@@ -5,18 +5,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+//import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class LaunchChrome {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Dell\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 		ChromeOptions co=new ChromeOptions();
+		//co.setHeadless("--headless");
 		co.setBinary("C:\\Users\\Dell\\Downloads\\chrome-win64 (1)\\chrome-win64\\chrome.exe");
+		//WebDriver driver=WebDriverManager.chromedriver().create();
 		WebDriver driver =new ChromeDriver(co);
 		driver.get("https://www.google.com");
 		driver.findElement(By.name("q")).sendKeys("Anil");
+		//driver.findElement(By.id("search")).sendKeys("Anil");
+		//driver.findElement(By.id("search-icon-legacy")).click();
 		System.out.println("Anil");
-		driver.close();
+		//driver.close();
 
 	}
 
